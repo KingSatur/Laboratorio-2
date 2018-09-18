bb7af319652e0176dc7c3f9d98e16e7b531eda35
Juan David Campillo Cepeda <38546862+KingSatur@users.noreply.github.com>
2018-09-18 16:24:36 -0500

    Changes in the interface

--- Project/src/controller/ControllerSystem.java
+++ Project/src/controller/ControllerSystem.java
@@ -1,22 +1,31 @@
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.input.MouseEvent;
 
-public class ControllerSystem {
+public class ControllerSystem implements Initializable{
 	
     @FXML
     private ComboBox<String> comboRanking;
     private ObservableList<String> lista;
    
+   
   
     @FXML
     void comboPres(MouseEvent event) {
+    }
+    
+    @FXML
+    public void initalize() {
     	lista = FXCollections.observableArrayList("1) Excelent skill and good latence", "2) Excelent skill and medium latence" , " 3) Excelent skill and bad latence" ,
     			"4) Excelent skill and pessimum latence" , "5) Good skill and good latence" , "6) Good skill and medium latence" , "7) Good skill and bad latence", 
     			"8) Good skill and pessimum latence" , "9) Medium skill and good latence" , "10) Medium skill and medium latence" , "11) Medium skill and bad latence",
@@ -25,6 +34,12 @@ public class ControllerSystem {
     			"20) Pessimum skill and pessimum latence");
     	comboRanking.setItems(lista);
     }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		initalize();
+		
+	}
     
 	
 	
